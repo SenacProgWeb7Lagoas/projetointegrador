891cd31f4a687817726e3fe19194a0764d7cc991
BrytanniADJ <brendaevil55@gmail.com>
2022-04-04 14:19:22 -0300

    Adicionei o modo noturno

---
+++ assests/js/alimentacao.js
@@ -0,0 +1,17 @@
+let ativarNoturno = true
+
+function modoNoturno(){
+    ativarNoturno = !ativarNoturno
+
+    if(ativarNoturno){
+        document.documentElement.style.setProperty('--body', '#3e3e3e');
+        document.documentElement.style.setProperty('--cabeca', '#252625');
+        document.documentElement.style.setProperty('--borda', '#165d71');
+        document.documentElement.style.setProperty('--texto', '#ffffff');
+    }else{
+        document.documentElement.style.setProperty('--body', '#c4c4c4');
+        document.documentElement.style.setProperty('--cabeca', '#ffffff');
+        document.documentElement.style.setProperty('--borda', '#0f7491');
+        document.documentElement.style.setProperty('--texto', '#000000');
+    }
+}
