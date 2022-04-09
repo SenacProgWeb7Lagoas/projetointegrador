0ef247eb7f9132a587f3f50ce5896260371fe699
Sávio_Francisco <saviofrancisco150@hotmail.com>
2022-04-09 11:25:24 -0300

    newScript

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,7 @@
-var  sum;
+var  sum; // Variável responsável por realizar as somas e retornar o resultado.
 
-function fristSection(){
+function fristSection(){ //Função que seleciona os valores da primeira pergunta.
+
     var value;
 
     if (document.getElementById("inSempre").checked === true) {
@@ -17,7 +18,8 @@ function fristSection(){
     }
     return value;
 }
-function secondSection(){
+function secondSection(){ // Função que seleciona os valores da segunda pergunta.
+
     var value2;
 
     if (document.getElementById("inSempre1").checked === true) {
@@ -34,27 +36,47 @@ function secondSection(){
     }
     return value2;
 }
-function trashBags(){
+function trashBags(){ // Função que seleciona o valor da última pergunta.
+
     var value3;
 
-    var inValue= document.getElementById("lixosforadecasa").value;
-
-    if(inValue === 1){
+    if(document.getElementById("lixosforadecasa").value === 1){
         value3 = 10;
     }
-    else if(inValue === 2){
+    else if(document.getElementById("lixosforadecasa").value === 2){
         value3 = 20;
     }
-    else{
+    else if(document.getElementById("lixosforadecasa").value >= 3){
         value3 = 30;
     }
-
+    else{
+        value3 = 0;
+    }
     return value3;
 }
+function selector(){ // Função que escolhe os valores da 3º Pergunta.
 
-function sendSum(){
+    var value4;
+    let valor=  document.getElementById("retiralixo").value;
+    
+    if(valor === "escolha" || valor === "sempre2"){
+        value4 = 0;
+    }
+    else if(valor === "asvezes2"){
+        value4 = 10;
+    }
+    else if(valor === "raramente2"){
+        value4 = 20;
+    }
+    else if(valor === "nunca2"){
+        value4 = 20;
+    }
+    return value4;
+} 
 
-    sum = trashBags() + secondSection() + fristSection();
+function sendSum(){ //Função para retornar a soma. 
+
+    sum = trashBags() + secondSection() + fristSection() + selector();
     alert ("A soma é: " + sum);
 }
 // Modo Light e Dark-mode
