630010a14fe9f5d3a9049713131b8b939bfaed1e
marcosfmd <marcosfelipemaia16@gmail.com>
2022-04-22 20:18:17 -0300

    Sugestões de layout para  pagina resultados.

---
+++ assests/js/resultado.js
@@ -0,0 +1,33 @@
+// Modo Light e Dark-mode
+
+let darkMode = document.querySelector (".color-model")
+
+    document.querySelector(".color-model").addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("change-color")
+    document.querySelector("body").classList.toggle("change-color")
+    document.querySelector("nav").classList.toggle("change-color")
+})
+
+// Muda o texto do botão de acordo com o modo
+
+let mudaText = true
+    
+        document.querySelector('.change-text').addEventListener('click', function (){
+        if (mudaText) {
+            mudaText = false
+            document.querySelector('.change-text').innerHTML = 'Dark Mode ON'
+        }else {
+            mudaText = true
+            document.querySelector('.change-text').innerHTML = 'Light Mode ON'
+        }
+})
+
+// Menu Hamburger
+let hamburger = document.querySelector (".hamburger");
+
+
+hamburger.addEventListener("click", function (){
+    document.querySelector("nav").classList.toggle("show-menu")
+    document.querySelector("body").classList.toggle("show-menu")
+    document.querySelector(".hamburger").classList.toggle("show-menu")
+})
