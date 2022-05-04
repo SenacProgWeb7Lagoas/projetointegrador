447385257bdc29a647833533ce75bc77a265c42f
HD OS <hdos@iMac-de-HD.local>
2022-05-03 21:54:50 -0300

    Valores

--- assests/js/transporte.js
+++ assests/js/transporte.js
@@ -4,13 +4,13 @@ function dMode() {
     const conQuestions = document.getElementById('contain_transporte');
     const atual = document.getElementById('modeColor').value;
 
-    if (atual == 'escuro') {
-            document.getElementById('dmode').innerHTML = `<input class="darkM"  type="button" value="CLARO" id="modeColor" onclick="dMode()">`
+    if (atual == 'Escuro') {
+            document.getElementById('dmode').innerHTML = `<input class="darkM"  type="button" value="Claro" id="modeColor" onclick="dMode()">`
             mainC.style.background = '#3E3E3E';
             selPage.style.background = '#252625';
             conQuestions.style.background = '#252625';        
     } else {
-            document.getElementById('dmode').innerHTML = `<input class="darkM"  type="button" value="escuro" id="modeColor" onclick="dMode()">`
+            document.getElementById('dmode').innerHTML = `<input class="darkM"  type="button" value="Escuro" id="modeColor" onclick="dMode()">`
             mainC.style.background = '#C4C4C4';
             selPage.style.background = 'white';
             conQuestions.style.background = 'white';
@@ -21,15 +21,18 @@ function saida() {
         const veiculo = document.getElementById('veiculo');
         const infoVeiculo = veiculo.options[veiculo.selectedIndex].value;
         const TrabalhoEmprego = document.getElementById('TrabalhoEmprego');
-        const infoTrabalho = veiculo.options[veiculo.selectedIndex].value;
+        const infoTrabalho = TrabalhoEmprego.options[TrabalhoEmprego.selectedIndex].value;
         const percorreDia = document.getElementById('percorreDia');
-        const infoPercorrer = veiculo.options[veiculo.selectedIndex].value;
+        const infoPercorrer = percorreDia.options[percorreDia.selectedIndex].value;
         const ferias = document.getElementById('ferias');
-        const infoFerias = veiculo.options[veiculo.selectedIndex].value;
+        const infoFerias = ferias.options[ferias.selectedIndex].value;
         const fimdeSemana = document.getElementById('fimdeSemana');
-        const infoFimSemana = veiculo.options[veiculo.selectedIndex].value;
-
-        document.getElementById('saida').innerHTML = `
-        'veiculo':'${infoVeiculo}'
-        `
+        const infoFimSemana = fimdeSemana.options[fimdeSemana.selectedIndex].value;
+        const somatransporte = parseInt(infoFimSemana) + parseInt(infoVeiculo) + parseInt(infoTrabalho) + parseInt(infoPercorrer) + parseInt(infoFerias);
+        alert(somatransporte)
+        localStorage.setItem('transporte', `${somatransporte}`);
+        location.href = '/consumo.html';
+        
+        
+        
 }
