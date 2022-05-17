f7b6efa23003e719ce967f6f0badd19803e68b41
SenacProgWeb7Lagoas <wis.tech7l@gmail.com>
2022-05-17 19:46:33 -0300

    adquações js

--- assests/js/alimentacao.js
+++ assests/js/alimentacao.js
@@ -6,10 +6,10 @@ let soma = 0
 
 
 
-function modoNoturno(){
+function modoNoturno() {
     ativarNoturno = !ativarNoturno
 
-    if(!ativarNoturno){
+    if (!ativarNoturno) {
         document.documentElement.style.setProperty('--body', '#3e3e3e');
         document.documentElement.style.setProperty('--cabeca', '#252625');
         document.documentElement.style.setProperty('--borda', '#165d71');
@@ -18,9 +18,9 @@ function modoNoturno(){
         document.documentElement.style.setProperty('--rosa', '#b12262');
         document.documentElement.style.setProperty('--laranja', '#b44a28');
         document.documentElement.style.setProperty('--roxo', '#5d335c');
-        document.querySelector(".noturno").innerHTML ="Light Mode";
-        
-    }else{
+        document.querySelector(".noturno").innerHTML = "Light Mode";
+
+    } else {
         document.documentElement.style.setProperty('--body', '#c4c4c4');
         document.documentElement.style.setProperty('--cabeca', '#ffffff');
         document.documentElement.style.setProperty('--borda', '#0f7491');
@@ -29,14 +29,14 @@ function modoNoturno(){
         document.documentElement.style.setProperty('--rosa', '#ed217c');
         document.documentElement.style.setProperty('--laranja', '#f15a29');
         document.documentElement.style.setProperty('--roxo', '#753874');
-         document.querySelector(".noturno").innerHTML ="Dark Mode";
+        document.querySelector(".noturno").innerHTML = "Dark Mode";
     }
 }
 
 
 
 //Responsividade
-function mobile(){
+function mobile() {
     document.querySelector(".cabecalho").classList.toggle("show-menu")
     document.querySelector(".nav").classList.toggle("show-menu")
     document.querySelector("header").classList.toggle("show-menu")
@@ -45,92 +45,70 @@ function mobile(){
 //Fim responsividade
 
 //Função para a 3º pergunta
-const escolha = function(){
+const escolha = function () {
     let radios = document.getElementsByName("pergunta2");
-    for(let i = 0; i<radios.length; i++){
-        if(radios[i].c){
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].c) {
             let marca = radios[i].value
-            for(let i = 0; i<radios.length; i++){
-                if(marca == 'sim'){
-                    result3=+ 25;
+            for (let i = 0; i < radios.length; i++) {
+                if (marca == 'sim') {
+                    result3 = + 25;
 
-                return
-                }else if(marca == 'nao'){
-                    result3=+ 125
-                   
                     return
-                }else if(marca == 'as_vezes'){
-                    result3=+ 50
-                  
+                } else if (marca == 'nao') {
+                    result3 = + 125
+
                     return
-                }else if(marca == 'raramente'){
-                    result3=+ 100
-                    
+                } else if (marca == 'as_vezes') {
+                    result3 = + 50
+
                     return
-                }else{
+                } else if (marca == 'raramente') {
+                    result3 = + 100
+
+                    return
+                } else {
                     alert("Responda a terceira pergunta!")
                 }
             }
-           
-                }   
-      }
+
         }
+    }
+}
 
 //1º pergunta
-function pergunta1(){
+function pergunta1() {
     let p1 = document.getElementById("box-alimen_p1");
-    for (i = 0; i < p1.length; i++) {
-        if(p1.selectedIndex == "0"){
-            alert("Escolha uma das opções para a primeira pergunta!")
-            return
-        }else if(p1.selectedIndex == "1"){
-            result1 = 0;
-            return
-        }else if(p1.selectedIndex == "2"){
-            result1 = 10;
-            return
-        }else if(p1.selectedIndex == "3"){
-            result1 = 20;
-            return
-        }else if(p1.selectedIndex == "4"){
-            result1 = 35;
-            return
-        }else if(p1.selectedIndex == "5"){
-            result1 = 50;
-            return
-        }
-}
+    let resposta = p1.value;
+    if(resposta === '-1'){
+        alert("Responda a pergunta 1!");
+    }else {
+        return parseInt(resposta);
+    }
 }
 //Função pergunta 2
-function pergunta2(){
+function pergunta2() {
     let p2 = document.getElementById("alimentacao_p2");
-    for (i = 0; i < p2.length; i++){
-        if(p2.selectedIndex == "0"){
-            alert("Selecione uma opção para a segunda pergunta!")
-            return
-        }else if(p2.selectedIndex == "1"){
-            result2 = 25;
-            return
-        }else if(p2.selectedIndex == "2"){
-            result2= 20;
-            return
-        }else if(p2.selectedIndex == "3"){
-            result2 = 15;
-            return
-        }else if(p2.selectedIndex == "4"){
-            result2 = 10;
-            return
-        }
+    console.log(p2);
+    let resposta = p2.value;
+    console.log('> ', resposta);
+    if(resposta === '0'){
+        let label2 = document.getElementById('label_pegunta2');
+        console.log('>> ', label2);
+        alert("Responda a pergunta 2!");
+    }else {
+        return parseInt(resposta);
+    }
 }
+//Função soma os 3 resultados das respostas
+function resultado() {
+    escolha();
+    pergunta1();
+    result2 = pergunta2();
+    soma = result1 + result2 + result3;
+    alert("Resultado:" + soma);
+    if(soma !== NaN){
+        localStorage['alimentacao'] = soma;
+        window.location.assign('./transporte.html');
+    }
 }
-        //Função soma os 3 resultados das respostas
-        function resultado(){
-            escolha();
-            pergunta1();
-            pergunta2();
-            if(p2.selectedIndex == "0" || p1.selectedIndex == "0" || radios[i].value == "" ){
-                alert("Responda todas as perguntas!");
-            }
-            soma = result1 + result2 + result3;
-            alert("Resultado:" + soma);
-        }
