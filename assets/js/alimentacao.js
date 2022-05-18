49ed7420272d3ecedf3bf787a1e38e47681d7258
SenacProgWeb7Lagoas <wis.tech7l@gmail.com>
2022-05-17 21:32:07 -0300

    resolve conflicts

--- assets/js/alimentacao.js
+++ assets/js/alimentacao.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let ativarNoturno = true;
 let result3 = 0
 let result1 = 0
@@ -11,14 +10,6 @@ function modoNoturno() {
     ativarNoturno = !ativarNoturno
 
     if (!ativarNoturno) {
-=======
-let ativarNoturno = true
-
-function modoNoturno(){
-    ativarNoturno = !ativarNoturno
-
-    if(ativarNoturno){
->>>>>>> transporte
         document.documentElement.style.setProperty('--body', '#3e3e3e');
         document.documentElement.style.setProperty('--cabeca', '#252625');
         document.documentElement.style.setProperty('--borda', '#165d71');
@@ -27,13 +18,8 @@ function modoNoturno(){
         document.documentElement.style.setProperty('--rosa', '#b12262');
         document.documentElement.style.setProperty('--laranja', '#b44a28');
         document.documentElement.style.setProperty('--roxo', '#5d335c');
-<<<<<<< HEAD
         document.querySelector(".noturno").innerHTML = "Modo claro";
-
     } else {
-=======
-    }else{
->>>>>>> transporte
         document.documentElement.style.setProperty('--body', '#c4c4c4');
         document.documentElement.style.setProperty('--cabeca', '#ffffff');
         document.documentElement.style.setProperty('--borda', '#0f7491');
@@ -42,25 +28,9 @@ function modoNoturno(){
         document.documentElement.style.setProperty('--rosa', '#ed217c');
         document.documentElement.style.setProperty('--laranja', '#f15a29');
         document.documentElement.style.setProperty('--roxo', '#753874');
-<<<<<<< HEAD
         document.querySelector(".noturno").innerHTML = "Modo escuro";
-=======
->>>>>>> transporte
     }
 }
-
-function mobile(){ // função acionada quando se clica no menu em barras ou no X
-
-    // adiciona a class showmenu no elemento que não tem essa classe e retira show-menu no elemento que contem
-    // ou seja alternar entre adicionar e retirar  
-    document.querySelector(".cabecalho").classList.toggle("show-menu")
-    document.querySelector(".nav").classList.toggle("show-menu")
-    document.querySelector("header").classList.toggle("show-menu")
-    document.querySelector(".logo").classList.toggle("show-menu")
-}
-
-
-<<<<<<< HEAD
 
 //Responsividade
 function mobile() {
@@ -139,5 +109,4 @@ function resultado() {
         window.location.assign('./transporte.html');
     }
 }
-=======
->>>>>>> transporte
+
