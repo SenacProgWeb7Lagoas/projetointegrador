16fa59c41792707970cbe2d2835f6116cbb78de2
SenacProgWeb7Lagoas <wis.tech7l@gmail.com>
2022-06-07 21:51:26 -0300

    update

--- assets/js/moradias.js
+++ assets/js/moradias.js
@@ -1,13 +1,13 @@
-function hamburger(){
-    let menuBurger = document.querySelector(".hamburger") 
+function hamburger() {
+    let menuBurger = document.querySelector(".hamburger")
     /*A variável acima recebe a função queryselector, que vai acessar documentos dentro da classe
      "hamburger", no html*/
-     /*queryselector sempre busca elementos dentro do html*/
-    
+    /*queryselector sempre busca elementos dentro do html*/
+
     menuBurger.addEventListener("click", hamburger)
     /*função que reproduz um som a partir de qualquer interação 
     com o elemento relacionado, no caso a função hamburger*/
-    
+
     document.querySelector(".hamburger").classList.toggle("show_menu")
     /*Função que muda a class da função hamburger quando há algum clique*/
 
@@ -18,14 +18,11 @@ function hamburger(){
     receberão a estilização atribuída à nova classe*/
 }
 
-hamburger()
+hamburger();
 
-function modoCor(){
-    let botaoCor = document.querySelector(".color-model")
-    botaoCor.addEventListener("click", modoCor)
-    document.querySelector(".color-model").classList.toggle("change_color")
+function modoCor() {
+    console.log('Test')
     document.querySelector("main").classList.toggle("change_color")
+    document.querySelector("nav").classList.toggle("change_color")
     document.querySelector("body").classList.toggle("change_color")
 }
-
-modoCor()
