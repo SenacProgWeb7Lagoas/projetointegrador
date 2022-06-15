71522b09eb482bff136359282cea12c889b0ed3e
Sávio_Francisco <saviofrancisco150@hotmail.com>
2022-06-15 20:27:57 -0300

    ChangesOfTheResultPag

--- assets/js/resultado.js
+++ assets/js/resultado.js
@@ -3,14 +3,15 @@ let outAlimentacao = document.getElementById("outAlimentação");
 let outTransporte = document.getElementById("outTransporte");
 let outConsumo = document.getElementById("outConsumo");
 let outResiduos = document.getElementById("outResiduos");
+let outTotal = document.getElementById("outTotal");
 
-
-const moradia = localStorage.getItem('moradia') || 0
-const alimentacao = localStorage.getItem('alimentacao') || 0
-const transporte = localStorage.getItem('trasnporte') || 0
+const moradia = localStorage.getItem('moradia') || 0; // operation to change the value of the constant.
+const alimentacao = localStorage.getItem('alimentacao') || 0;
+const transporte = localStorage.getItem('trasnporte') || 0;
 const consumo = localStorage.getItem('consumo') || 0;
 const residuos = localStorage.getItem('residuos') || 0;
 
+const total = parseInt(moradia + alimentacao + transporte + consumo + residuos); // realiza a soma das opções escolhidas pelo user
 
 outMoradia.innerHTML = moradia;
 outAlimentacao.innerHTML = alimentacao;
@@ -18,6 +19,7 @@ outTransporte.innerHTML = transporte;
 outConsumo.innerHTML = consumo;
 outResiduos.innerHTML = residuos;
 
+outTotal.innerHTML = total; // show in page the total of the options 
 
 
 // Modo Light e Dark-mode
@@ -35,15 +37,17 @@ let darkMode = document.querySelector (".color-model")
 
 let mudaText = true
     
-        document.querySelector('.change-text').addEventListener('click', function (){
+document.querySelector('.change-text').addEventListener('click', function (){
+
         if (mudaText) {
             mudaText = false
             document.querySelector('.change-text').innerHTML = 'Dark Mode ON'
         }else {
-            mudaText = true
-            document.querySelector('.change-text').innerHTML = 'Light Mode ON'
+                mudaText = true
+                document.querySelector('.change-text').innerHTML = 'Light Mode ON'
+            }
         }
-})
+    )
 
 // Menu Hamburger
 let hamburger = document.querySelector (".hamburger");
