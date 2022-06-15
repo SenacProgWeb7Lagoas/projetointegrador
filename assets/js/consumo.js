5428a38823af10ce5251b0fedc64593ffc601b91
Luíza Araújo <99282366+lh-ude@users.noreply.github.com>
2022-06-14 21:44:45 -0300

    update 14-16

--- assets/js/consumo.js
+++ assets/js/consumo.js
@@ -1,25 +1,63 @@
 let ativarNoturno = true
 
-function modoNoturno(){
+function modoNoturno() {
     ativarNoturno = !ativarNoturno
 
-    if(ativarNoturno){
+    if (ativarNoturno) {
         document.documentElement.style.setProperty('--body', '#3e3e3e');
         document.documentElement.style.setProperty('--cabeca', '#252625');
         document.documentElement.style.setProperty('--borda', '#165d71');
         document.documentElement.style.setProperty('--texto', '#ffffff');
         document.documentElement.style.setProperty('--verde', '#779451');
         document.documentElement.style.setProperty('--rosa', '#b12262');
-        document.documentElement.style.setProperty('--laranja', '#b44a28');
+        document.documentElement.style.setProperty('--laranja','#f15a29');
         document.documentElement.style.setProperty('--roxo', '#5d335c');
-    }else{
+    } else {
         document.documentElement.style.setProperty('--body', '#c4c4c4');
         document.documentElement.style.setProperty('--cabeca', '#ffffff');
         document.documentElement.style.setProperty('--borda', '#0f7491');
         document.documentElement.style.setProperty('--texto', '#000000');
         document.documentElement.style.setProperty('--verde', '#a6ce39');
         document.documentElement.style.setProperty('--rosa', '#ed217c');
-        document.documentElement.style.setProperty('--laranja', '#f15a29');
+        document.documentElement.style.setProperty('--laranja','#f15a29');
         document.documentElement.style.setProperty('--roxo', '#753874');
     }
 }
+var sum; // Variável responsável por realizar as somas e retornar o resultado.
+
+function firstSection() { //Função que seleciona os valores da primeira pergunta.
+
+    var value;
+
+    if (document.getElementById("inNenhuma").checked === true) {
+        value = 0;
+    }
+    else if (document.getElementById("in1 a 3").checked === true) {
+        value = 15;
+    }
+    else if (document.getElementById("in4 a 6").checked === true) {
+        value = 30;
+    }
+    else if (document.getElementById("inMais de 6").checked === true) {
+        value = 45;
+    }
+    return value;
+}
+function secondSection() { // Função que seleciona os valores da segunda pergunta.
+
+    var value2;
+
+    if (document.getElementById("inSim").checked === true) {
+        value2 = 0;
+    }
+    else if (document.getElementById("inNao").checked === true) {
+        value2 = 25;
+    }
+    return value2;
+}
+
+sum = firstSection() + secondSection();
+
+localStorage['consumo'] = sum;
+
+window.location.assign('./residuos.html');
