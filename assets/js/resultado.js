9a3cb7eb15db8e54d72d9101fe9149b314c7bf1c
Sávio_Francisco <saviofrancisco150@hotmail.com>
2022-06-21 20:48:53 -0300

    addResultadosTotalDeÁrvores

--- assets/js/resultado.js
+++ assets/js/resultado.js
@@ -5,6 +5,9 @@ let outConsumo = document.getElementById("outConsumo");
 let outResiduos = document.getElementById("outResiduos");
 let outTotal = document.getElementById("outTotal");
 
+let outHectares = document.getElementById("outHectares");
+let outArvores = document.getElementById("outArvores");
+
 const moradia = localStorage.getItem('moradia') || 0; // operation to change the value of the constant.
 const alimentacao = localStorage.getItem('alimentacao') || 0;
 const transporte = localStorage.getItem('trasnporte') || 0;
@@ -21,6 +24,51 @@ outResiduos.innerHTML = residuos;
 
 outTotal.innerHTML = total; // show in page the total of the options 
 
+if (total >= 0 && total <= 38){
+    outHectares.innerHTML = 1;
+    outArvores.innerHTML = 2500;
+    
+}else if(total >= 39 && total <= 75){
+    outHectares.innerHTML = 2;
+    outArvores.innerHTML = 5000;
+
+}else if(total >= 76 && total <= 113){
+    outHectares.innerHTML = 3;
+    outArvores.innerHTML = 7500;
+
+}else if(total >= 114 && total <= 150){
+    outHectares.innerHTML = 4;
+    outArvores.innerHTML = 10000;
+
+}else if(total >= 151 && total <= 275){
+    outHectares.innerHTML = 5;
+    outArvores.innerHTML = 12500;
+
+}else if(total >= 276 && total <= 400){
+    outHectares.innerHTML = 6;
+    outArvores.innerHTML = 15000;
+
+}else if(total >= 401 && total <= 500){
+    outHectares.innerHTML = 7;
+    outArvores.innerHTML = 17500;
+
+}else if(total >= 501 && total <= 600){
+    outHectares.innerHTML = 8;
+    outArvores.innerHTML = 20000;
+
+}else if(total >= 601 && total <= 700){
+    outHectares.innerHTML = 9;
+    outArvores.innerHTML = 22500;
+
+}else if(total >= 701 && total <= 800){
+    outHectares.innerHTML = 10;
+    outArvores.innerHTML = 25000;
+
+}else if(total >= 801 && total <= 855){
+    outHectares.innerHTML = 11;
+    outArvores.innerHTML = 275000;
+
+}
 
 // Modo Light e Dark-mode
 
@@ -51,7 +99,6 @@ document.querySelector('.change-text').addEventListener('click', function (){
 
 // Menu Hamburger
 let hamburger = document.querySelector (".hamburger");
-
 
 hamburger.addEventListener("click", function (){
     document.querySelector("nav").classList.toggle("show-menu")
