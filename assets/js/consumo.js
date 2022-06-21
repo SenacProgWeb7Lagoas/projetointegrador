d22d21b1b1e68442b90553da2cc2fe9c331494ce
Luíza Araújo <99282366+lh-ude@users.noreply.github.com>
2022-06-21 20:21:44 -0300

    update 21-06

--- assets/js/consumo.js
+++ assets/js/consumo.js
@@ -1,4 +1,4 @@
-let ativarNoturno = true
+let ativarNoturno = false
 
 function modoNoturno() {
     ativarNoturno = !ativarNoturno
@@ -6,20 +6,17 @@ function modoNoturno() {
     if (ativarNoturno) {
         document.documentElement.style.setProperty('--body', '#3e3e3e');
         document.documentElement.style.setProperty('--cabeca', '#252625');
-        document.documentElement.style.setProperty('--borda', '#165d71');
         document.documentElement.style.setProperty('--texto', '#ffffff');
         document.documentElement.style.setProperty('--verde', '#779451');
         document.documentElement.style.setProperty('--rosa', '#b12262');
-        document.documentElement.style.setProperty('--laranja','#f15a29');
         document.documentElement.style.setProperty('--roxo', '#5d335c');
-    } else {
+    } 
+    else {
         document.documentElement.style.setProperty('--body', '#c4c4c4');
         document.documentElement.style.setProperty('--cabeca', '#ffffff');
-        document.documentElement.style.setProperty('--borda', '#0f7491');
         document.documentElement.style.setProperty('--texto', '#000000');
         document.documentElement.style.setProperty('--verde', '#a6ce39');
         document.documentElement.style.setProperty('--rosa', '#ed217c');
-        document.documentElement.style.setProperty('--laranja','#f15a29');
         document.documentElement.style.setProperty('--roxo', '#753874');
     }
 }
@@ -56,8 +53,10 @@ function secondSection() { // Função que seleciona os valores da segunda pergu
     return value2;
 }
 
+object.onclick function resultados() { 
 sum = firstSection() + secondSection();
 
 localStorage['consumo'] = sum;
 
 window.location.assign('./residuos.html');
+}
