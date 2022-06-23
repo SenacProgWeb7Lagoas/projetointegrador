91aff241050dfab77a2647a3d6e1fa17434478a0
hd <hd@iMac-de-hd.local>
2022-06-23 07:37:18 -0300

    comlete

--- assets/js/transporte.js
+++ assets/js/transporte.js
@@ -1,83 +1,88 @@
 
 function dMode() {
     const mainC = document.getElementById('mainD');
+    var spanElements = document.getElementsByTagName('span');
+
+       
+    
+   
     const selPage = document.getElementById('contain_selectPage');
+    console.log(selPage);
     const conQuestions = document.getElementById('contain_transporte');
     const conQ = document.getElementById('contain_quests');
     
     const atual = document.getElementById('modeColor').value;
 
     if (atual == 'Modo escuro') {
+
+            for (var i = 0; i < spanElements.length; i++) {
+                spanElements[i].style.color = 'white';
+                }
+
             document.getElementById('dmode').innerHTML = `<input class="darkM"  type="button" value="Modo Claro" id="modeColor" onclick="dMode()">`
+            
+           
             mainC.style.background = '#3E3E3E';
             selPage.style.background = '#252625';
             conQuestions.style.background = '#ED217C';      
             conQ.style.background =   '#252625';
+            
             document.documentElement.style.setProperty('--perguntas', '#d6d6d6');
+            
     } else {
+                for (var i = 0; i < spanElements.length; i++) {
+                spanElements[i].style.color = 'black';
+                }
             document.getElementById('dmode').innerHTML = `<input class="darkM"  type="button" value="Modo escuro" id="modeColor" onclick="dMode()">`
             mainC.style.background = '#C4C4C4';
+            
             selPage.style.background = '#ED217C';
             conQuestions.style.background = '#ED217C';
             conQ.style.background =   'white';
+            
             document.documentElement.style.setProperty('--perguntas', 'rgb(40, 40, 40)');
+            
     }
 
 }
 
+function marcaVeiculo(caller) {
+        var checks = document.querySelectorAll('input[type="checkbox"]');    
+        for(let i = 0; i < checks.length; i++) {
+          checks[i].checked = checks[i] == caller;   
+        }
+      }
+
+      function marca(caller) {
+        var checks = document.querySelectorAll('input[type="checkbox"]');    
+        for(let i = 0; i < checks.length; i++) {
+          checks[i].checked = checks[i] == caller;   
+        }
+      }
 
 
         
 
 function saida() {
-        const selecVeiculo = document.querySelector('input[name=viajar]:checked').value
-        const selecPercorre = document.querySelector('input[name=percorre]:checked').value
+        const selecVeiculo = document.querySelector('input[name=veiculo]:checked').value;
+        const selecPercorre = document.querySelector('input[name=percorre]:checked').value;
+        const selecViajar = document.querySelector('input[name=viajar]:checked').value;
 
-        const selecViajar = document.querySelector('input[name=viajar]:checked').value
-        
-        console.log(selecPercorre);
-        console.log(selecViajar);
-        console.log(selecVeiculo);
-        
-        const veiculo = document.getElementById('veiculo');
-        const infoVeiculo = veiculo.options[veiculo.selectedIndex].value;
         const TrabalhoEmprego = document.getElementById('TrabalhoEmprego');
-        const infoTrabalho = TrabalhoEmprego.options[TrabalhoEmprego.selectedIndex].value;
+        const infoTrabalho = TrabalhoEmprego.options[TrabalhoEmprego.selectedIndex].value;     
         
         const ferias = document.getElementById('ferias');
         const infoFerias = ferias.options[ferias.selectedIndex].value;
-        const fimdeSemana = document.getElementById('fimdeSemana');
-        const infoFimSemana = fimdeSemana.options[fimdeSemana.selectedIndex].value;
-        const somatransporte = parseInt(infoFimSemana) + parseInt(infoVeiculo) + parseInt(infoTrabalho) + parseInt(infoPercorrer) + parseInt(infoFerias);
-        if (infoVeiculo == "" ||  infoTrabalho == "" || infoPercorrer == "" || infoFerias == "" || infoFimSemana == "" ) {
-                alert (`POR FAVOR, PREENCHA TODAS AS INFORMAÇÕES`);
-                if(infoVeiculo == "") {
-                        var p1 = "01";
-                        alert(`RESPONDA A PERGUNTA DE NUMERO ${p1}`);
-                };
-                if(infoTrabalho == "") {
-                        var p2 = "02";
-                        alert(`RESPONDA A PERGUNTA DE NUMERO ${p2}`);
-                };
-                if(infoPercorrer == "") {
-                        var p3 = "03"
-                        alert(`RESPONDA A PERGUNTA DE NUMERO ${p3}`);
-                        alert("Cansou né, então responda de uma vez todas as perguntas.")
-                };
-                if(infoFerias == "") {
-                        var p4 = "04";
-                        alert(`RESPONDA A PERGUNTA DE NUMERO ${p4}`);
-                };
-                if(infoFimSemana == "") {
-                        var p5 = "05"
-                        alert(`RESPONDA A PERGUNTA DE NUMERO ${p5}`);
-                };
-                
 
-        } else {                 
+        
+        const somatransporte = parseInt(selecVeiculo) + parseInt(selecPercorre) + parseInt(selecViajar) + parseInt(infoTrabalho) + parseInt(infoFerias);
+        console.log(somatransporte)
+
+
+                        
         localStorage.setItem('transporte', `${somatransporte}`);
         location.href = '/consumo.html';
-        }
+        
         
         
         
